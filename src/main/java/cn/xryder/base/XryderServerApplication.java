e0049f1aef50ae4500e18d18a61f5e9d3a1f72e6
joetao <cutesimba@163.com>
2024-12-01 19:48:46 +0800

    init

---
+++ src/main/java/cn/xryder/base/XryderServerApplication.java
@@ -0,0 +1,13 @@
+package cn.xryder.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XryderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XryderServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xryder/base/XryderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xryder.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XryderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
