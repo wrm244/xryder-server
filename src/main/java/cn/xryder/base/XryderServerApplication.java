e8e20241b381c6b66f05cb80d7e7928410acd7bc
wuheshan <wuheshan@tisson.cn>
2025-07-08 23:07:54 +0800

    feat(config): 添加 Tomcat 和日志配置
    - 在 application.yml 中添加 Tomcat 配置
    - 新增 logback-spring.xml 日志配置文件
    - 添加虚拟线程异步配置
    - 更新 PDF 读取方式
    - 重构 JWT 服务
    - 优化用户服务实现

--- src/main/java/cn/xryder/base/XryderServerApplication.java
+++ src/main/java/cn/xryder/base/XryderServerApplication.java
@@ -2,23 +2,9 @@ package cn.xryder.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
-import java.util.concurrent.Executors;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 /**
- * @Author: joetao
- * @Date: 2024/8/1 14:57
- * @Email: cutesimba@163.com
+ * @author wrm244
  */
 @SpringBootApplication
 public class XryderServerApplication {
